1d677a3148f67f6c103f8335972b8dc21c7fd51b
test <33888474+xiaokanghub@users.noreply.github.com>
2019-01-15 13:56:45 +0700

    Add files via upload

---
+++ Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+ 
+include $(CLEAR_VARS)
+ 
+LOCAL_MODULE := inject
+LOCAL_SRC_FILES := inject.c
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE 
+LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog
+include $(BUILD_EXECUTABLE)
